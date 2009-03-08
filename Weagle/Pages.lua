ba4eb5af21f3bcf3b013ed76ea17d4006759474b
Jerome Leclanche <jerome@leclan.ch>
2009-03-08 02:45:39 +0200

    Update Pages to latest version

--- Weagle/Pages.lua
+++ Weagle/Pages.lua
@@ -1,5 +1,100 @@
 pages = {
 	["ITEM: Bookmarks"] = {
+		23458, -- OK
+		31583, -- OK
+		21588, -- Wristguards of Elemental Fury
+		31552, -- OK
+		31560, -- OK
+		35518, -- OK
+		35533, -- OK
+		35546, -- OK
+		35204, -- OK
+		31568, -- OK
+		35530, -- OK
+		35553, -- OK
+		35520, -- OK
+		35517, -- OK
+		19297, -- OK
+		35531, -- OK
+		35525, -- OK
+		35523, -- OK
+		7986,  -- Plans: Ornate Mithril Breastplate
+		6343,  -- Formula: Imbue Chest - Spirit
+		6345,  -- Formula: Imbue Cloak - Protection
+		32972, -- Beer Goggles, WoWDB
+		6834,  -- Black Tuxedo, WoWDB
+		41757, -- Vest of Unyielding Companionship
+		38317, -- Prepared Drakkari Offering
+		39709, -- Verdant Tundra Boots
+		15769, -- Pattern: Onyxia Scale Cloak
+		23461, -- High Warlord's Quickblade
+		1217,  -- Unknown Reward
+		36967, -- Grimoire of Fire Shield (Rank 7)
+		27971, -- TEST 130 Epic Paladin DPS Necklace
+		4116,  -- Olmann Sewar (RFG)
+		17968, -- Charged Scale of Onyxia
+		5005,  -- Emberspark Pendant
+		33797, -- Portable Brewfest Keg
+		34805, -- QA Test Ring +100 Spell Hit Rating
+		7548,  -- Onyx Choker
+		21587, -- Wristguards of Castigation
+		37485, -- Clam Collector Gauntlets
+		37311, -- Skybreaker Whip
+		37312, -- Carrot on a Stick
+		37313, -- Riding Crop
+		40355, -- Azure Whelpling
+		17000, -- Band of the Wraith
+		2486,  -- Large Stone Mace
+		36919, -- Cardinal Ruby
+		28045, -- Cloak of the Demon Stalker
+		22781, -- Polar Bear Collar
+		37878, -- Worg's Blood Elixir
+		43097, -- Fur Lining - Attack Power
+		36897, -- Fel Spellstone
+		7551,  -- Entwined Opaline Talisman
+		41741, -- Cobalt Rod
+		6130,  -- Trapper's Shirt
+		2444,  -- Ornate Buckler
+		2442,  -- Reinforced Targe
+		27719, -- Aldor Leggings of Puissance
+		30203, -- Mantle of the Fire Festival
+		32148, -- Chancellor's Ringmail Chestguard
+		37400, -- Whalehunter Cuffs
+		36967, -- Grimoire of Consume Shadows (Rank 9)
+		34661, -- Massacre Sword
+		40582, -- Scourgestone
+		43517, -- Penguin Egg
+		24315, -- Pattern: Heavy Netherweave Net
+		7426,  -- Cerulean Ring
+		7996,  -- Lucky Fishing Hat
+		41585, -- Slag Covered Metal
+		24288, -- Greater Spellthread
+		4196,  -- Feathered Mantle
+		23885, -- Schematic: Remote Mail Terminal
+		5000,  -- Coral Band
+		20364, -- Test Ammo Lockbox
+		41756, -- Heart's Blood Signet
+		6589,  -- Viridian Band
+		37225, -- Sweltering Sandals
+		27718, -- Aldor Defender's Legplates
+		1193,  -- Banded Buckler
+		2413,  -- Palomino
+		
+		25627, -- QR 9922 Shield
+		-- Magelo
+		21168, -- Baby Shark
+		37706, -- Hardened Saronite Bar
+		38640, -- Dense Frostweave Bandage
+		39153, -- Manual: Dense Frostweave Bandage
+		43733, -- Crown of Faith
+		43739, -- Dreadnaught Battleplate
+		
+		31469, -- q10787
+		37827, -- Brewfest Kodo, a303
+		
+		25968,
+		25969,
+		25970,
 		37919, -- Test Scott K Bow
 		44924, -- Arcanite Ripper (NEW)
 		2496,  -- Raider Shortsword
@@ -7,7 +102,6 @@ pages = {
 		34661, -- Massacre Sword
 		36919, -- Cardinal Ruby
 		40534, -- Monster - Axe, 1H Northrend C02 Blue
-		1281,
 		2415,  -- White Stallion, Allakhazam
 		2499,  -- Double-bladed Axe
 		5105,  -- Explosive Shell
@@ -30,23 +124,27 @@ pages = {
 		23388, -- Tranquillien Tabard
 		23855, -- Spellfire Cloth
 		25677, -- Terokkar Chokeberry, Thottbot
+		32876, -- Monster - Black Temple - Crossbow - Dragonmaw Sky Stalker
 		32911, -- Empty Brewfest Stein
 		33007, -- Grimbooze's Secret Recipe
 		33063, -- Really Tough Brewfest Bread
 		33957, -- Witches Band
+		32457, -- Arakkoa Fetish
 		37740, -- Brutal Gladiator's Swift Judgement
 		38282, -- Bland Two-Handed Blade
+		38587, -- Empty Brewfest Stein
 		30845, -- Glyph of Chromatic Warding
 --		37298, -- Competitor's Souvenir
 --		22045, -- Test QARaid Uber Ammo Lockbox
 --		29887, -- Hunter 150 Epic Ammo Box
+		5663,  -- Horn of the Red Wolf
 		--8627,  -- Reins of the Night saber
 --		8628,  -- Reins of the Spotted Nightsaber
 --		8629,  -- Reins of the Striped Nightsaber
-		--8630,  -- Reins of the Bengal Tiger
+		8630,  -- Reins of the Bengal Tiger
 --		8621,  -- Reins of the Striped Frostsaber
 --		8632,  -- Reins of the Spotted Frostsaber
-		--8633,  -- Reins of the Leopard
+		8633,  -- Reins of the Leopard
 		--12325, -- Reins of the Primal Leopard
 		--12326, -- Reins of the Tawny Sabercat
 		--12327, -- Reins of the Golden Sabercat
@@ -57,6 +155,110 @@ pages = {
 --		20965, -- Cut Ruby
 --		21772, -- Cut Sapphire
 --		21773, -- Cut Opal
+	},
+--	["ITEM: Deadlinks"] = {
+--		7948,  -- is221
+--		7949,  -- is221
+--		7950,  -- is221
+--		7951,  -- is221
+--		7952,  -- is221
+--		7953,  -- is221
+--	},
+	["ITEM: Magelo Arena"] = {
+		42217,
+		42222,
+		42618,
+		42512,
+		42574,
+		44416,
+		42221,
+		42296,
+		42226,
+		42587,
+		42619,
+		42851,
+		42582,
+		42601,
+		42575,
+		42254,
+		42231,
+		42215,
+		42596,
+		42529,
+		42606,
+		42269,
+		42359,
+		42535,
+		42212,
+		42213, -- LK Arena 1 Fast Offhand Axe
+		42224,
+		42295,
+		42274,
+		42568,
+		42223,
+		42259,
+		42219,
+		42530,
+		42343,
+		42207,
+		42558,
+		42494,
+		42289,
+		42556,
+		42279,
+		42264,
+		42536,
+		42321,
+		42316,
+		42241,
+		42613,
+		42445,
+		42576,
+		42351,
+		42447,
+		42247,
+		42297,
+		42577,
+		42444,
+		42449,
+		42484,
+		42356,
+		42557,
+		42220,
+		42517,
+		42523,
+		42383,
+		42331,
+		42284,
+		42218,
+		42448,
+		42569,
+		42345,
+		42344,
+		42563,
+		42388,
+		
+	},
+	["ITEM: Tournament - TBC"] = {
+		35400, -- Enchant Boots - Dexterity
+		35417, -- Enchant Boots - Fortitude
+		35423, -- Enchant Bracer - Major Intellect
+		35424, -- Enchant Bracer - Restore Mana Prime
+		35427, -- Enchant Bracer - Superior Healing
+		35429, -- Enchant Chest - Exceptional Stats
+		35432, -- Enchant Cloak - Greater Agility
+		35433, -- Enchant Cloak - Greater Arcane Resistance
+		35435, -- Enchant Cloak - Major Resistance
+		35440, -- Enchant Gloves - Major Healing
+		35441, -- Enchant Gloves - Major Spellpower
+		35444, -- Enchant Ring - Healing Power
+		35447, -- Enchant Ring - Striking
+		35448, -- Enchant Shield - Intellect
+		35454, -- Enchant Weapon - Major Healing
+		35728, -- Greater Inscription of the Blade
+		35729, -- Greater Inscription of the Knight
+		35730, -- Greater Inscription of the Oracle
+		35731, -- Greater Inscription of the Orb
 	},
 	["ITEM: Beta JWC Designs"] = {
 		23130, -- Design: Teardrop Blood Garnet
@@ -73,22 +275,20 @@ pages = {
 		119,   -- Deprecated Rogue's Vest
 		128,   -- Deprecated Tauren Trapper's Pants
 		192,   -- Martin Thunder
+		1024,  -- Plate Helmet D2 (test)
 		1589,  -- Tablet of Spirit Armor III
 		2715,  -- Monster - Item, Lantern - Round
 		5148,  -- Book of Thorns II
 		5417,  -- Weapon of Massive Destruction (test)
 		5418,  -- Weapon of Mass Destruction (test)
-		6619,  -- Manual: The Path of Defense
-		13262, -- Ashbringer, Varimathras
 		14062, -- Kodo Mount
-		17142, -- Shard of the Defiler, Archimonde
 		17782, -- Talisman of Binding Shard
-		18964, -- Turtle Egg (Loggerhead), Suramar
-		37739, -- Brutal Gladiator's Blade of Alacrity, Archimonde
+		36519, -- Moonlit Katana, Bronze Dragonflight
 		44743, -- Cultist Acolyte's Hood Target
 		44946, -- Scroll of Enchant Weapon - Titanguard
 	},
 	["ITEM: Misc"] = {
+		1281,  -- Deprecated Quiver (TEST), Kil'Jaeden
 		6437,  -- Flayed Demon Skin (old)
 		6766,  -- Flayed Demon Skin (old2)
 		6836,  -- Dress Shoes
@@ -102,7 +302,7 @@ pages = {
 		20978, -- Apprentice's Staff
 		23162, -- Foror's Crate of Endless Resist Gear Storage
 		29805, -- Socrethar's Head
-		38605, -- McCarty's Notes, La Croisade Ecarlate
+		38605, -- McCarty's Notes
 		38538, -- Riding Crop
 		42440, -- Gigaflux Dimensional Transporter
 		43307, -- Arena Points
@@ -131,17 +331,21 @@ pages = {
 		2715,  -- Monster - Item, Lantern - Round
 		13337, -- Monster - Staff, Feathered Gold
 		14586, -- Monster - Mace2H, Fist of Omokk
+		32876, -- Monster - Black Temple - Crossbow - Dragonmaw Sky Stalker
 		39754, -- Monster - Sword, Northrend Iron Dwarf
 		40534, -- Monster - Axe, 1H Northrend C02 Blue
 	},
 	["ITEM: Brew of the Month"] = {
 		37488, -- Wild Winter Pilsner
 		37489, -- Izzard's Ever Flavor
+		37490, -- Aromatic Honey Brew
 		37496, -- Binary Brew
 		37497, -- Autumnal Acorn Ale
 		37498, -- Bartlett's Bitter Brew
 		37499, -- Lord of Frost's Private Label
+		37898, -- Wild Winter Pilsner
 		37899, -- Izzard's Ever Flavor
+		37900, -- Aromatic Honey Brew
 		37906, -- Binary Brew
 		37909, -- Lord of Frost's Private Label
 	},
@@ -157,6 +361,8 @@ pages = {
 		24269, -- Heavy Netherweave Net
 		25884, -- Primal Stone Statue
 		29052, -- Warp Nether Extractor
+		32594, -- Ogrela Test Tunic
+		32595, -- Ogrela Reagent Dust
 		34144, -- Branch of Destruction
 		34145, -- Rod of Devastation
 		34146, -- Red Death
@@ -194,6 +400,7 @@ pages = {
 		10590, -- Pocked Black Box
 		11743, -- Rockfist
 		13173, -- Broken Flightblade Throwing Axe
+		13254, -- Astral Guard
 		13602, -- Greater Spellstone (DEPRECATED)
 		13603, -- Major Spellstone (DEPRECATED)
 		13699, -- Firestone (DEPRECATED)
@@ -231,8 +438,11 @@ pages = {
 		33012, -- Terokkar Lilac
 		33014, -- Fine Poking Stick
 		33976, -- Brewfest Ram
+		33978, -- "Honorary Brewer" Hand Stamp
+		34028, -- "Honorary Brewer" Hand Stamp
 	},
 	["ITEM: RFG - Naxx"] = {
+		21588, -- Wristguards of Elemental Fury
 		22520, -- The Phylactery of Kel'Thuzad
 		22589, -- Atiesh, Greatstaff of the Guardian
 		22682, -- Frozen Rune
@@ -248,18 +458,45 @@ pages = {
 		23663, -- Girdle of Elemental Fury
 		23665, -- Leggings of Elemental Fury
 	},
+	["ITEM: RFG - Honor"] = {
+		23458, -- High Warlord's Spellblade
+		23459, -- High Warlord's Battle Mace
+		28833, -- High Warlord's Lamellar Helm
+		28834, -- High Warlord's Lamellar Legguards
+		28942, -- Grand Marshal's Bonegrinder
+	},
+	["ITEM: Bloodstained Fortune"] = {
+		32689,
+		32692,
+	},
+	["ITEM: PTR QA"] = {
+		32762, -- Rulkster's Brain Juice
+		32763, -- Rulkster's Secret Sauce
+		32764, -- Flaskataur's Celestial Brew
+		32765, -- Flaskataur's Tears
+		32766, -- Flaskataur's Flask of Flaskocity
+		32767, -- Flaskataur's Flask of Pretty Good Power
+	},
 	["ITEM: Misc - NTL"] = {
 		1386,  -- Thistlewood Axe, Emerald Dream EU
 		2377,  -- Round Buckler, Cenarion Circle US
 		4111,  -- Master Hunter's Rifle, Alexstrasza US
+		5623,  -- Amethyst Phial, Ravencrest EU
+		6619,  -- Manual: The Path of Defense, Bloodfeather
+		7994,  -- Plans: Orcish War Leggings, Bloodfeather
 		8628,  -- Reins of the Spotted Nightsaber, Alexstrasza US
-		9380,  -- Jang'thraze the Protector, Ysera (Archimonde?)
+		9380,  -- Jang'thraze the Protector, Archimonde
+		13262, -- Ashbringer, Saurfang
+		17142, -- Shard of the Defiler, Archimonde
+		18964, -- Turtle Egg (Loggerhead), Suramar
 		19112, -- Frozen Steel Vambraces, Windrunner/Grim Batol EU
 		22737, -- Atiesh, Greatstaff of the Guardian, Bronze Dragonflight EU / Suramar EU
 		25596, -- Peep's Whistle, Archimonde
 		25967, -- Eagle Crested Pauldrons, Draenor US
+		28596, -- Design: Bright Blood Garnet
 		32896, -- Scroll of the Sun, Silvermoon EU
 		33986, -- Indalamar's Ring of 200 Crit, Archimonde
+		37739, -- Brutal Gladiator's Blade of Alacrity, Archimonde
 		39754, -- Monster - Sword, Northrend Iron Dwarf, Aman'Thul
 	},
 	["ITEM: Stationeries"] = {
